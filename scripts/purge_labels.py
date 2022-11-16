dbdbc74a758bb82269fb75f4bcb22a3a9b347a0b
James Harvey <114482848+jharvey2556@users.noreply.github.com>
2022-11-16 11:47:50 +0000

    Update purge_labels.py

--- scripts/purge_labels.py
+++ scripts/purge_labels.py
@@ -2,9 +2,7 @@ import os
 import glob
 
 def main(labels_path):
-    response = input("purge labels folder Y/N ")
-    if response == "Y":
-        files = glob.glob(labels_path + "*")
-        for f in files:
-            os.remove(f)
+       files = glob.glob(labels_path + "*")
+       for f in files:
+           os.remove(f)
         
